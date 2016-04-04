bc60eecca919f7487bd31b0513d5621497505b4a
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-04-04 20:41:36 +0300

    Changed default time interval for job

--- jobs/appstore_random_review_job.rb
+++ jobs/appstore_random_review_job.rb
@@ -1,13 +1,8 @@
-require 'net/http'
-require 'json'
-
 require_relative '../lib/infrastructure/project_manager'
 require_relative '../lib/infrastructure/project_model'
-require_relative '../lib/appstore/rate_calculator'
 require_relative '../lib/appstore/review_service'
-require_relative '../lib/appstore/version_determinator'
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '30s', :first_in => 0 do |job|
   project_manager = Infrastructure::ProjectManager.new
   project_manager.obtain_all_projects.each do |project|
     service = AppStore::ReviewService.new
