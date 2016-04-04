77e541a9fbf861030e39e3b75f62f823630c7693
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-04-04 18:47:36 +0300

    Implemented sending really random review to a widget

--- jobs/appstore_random_review_job.rb
+++ jobs/appstore_random_review_job.rb
@@ -7,11 +7,18 @@ require_relative '../lib/appstore/rate_calculator'
 require_relative '../lib/appstore/review_service'
 require_relative '../lib/appstore/version_determinator'
 
-SCHEDULER.every '1d', :first_in => 0 do |job|
+SCHEDULER.every '5s', :first_in => 0 do |job|
   project_manager = Infrastructure::ProjectManager.new
   project_manager.obtain_all_projects.each do |project|
     service = AppStore::ReviewService.new
     reviews = service.obtain_reviews_for_app_id(project.appstore_id)
 
+    random_review = reviews.sample
+    widget_name = "appstore_review_#{project.appstore_id}"
+    send_event(widget_name, {
+                              'author-name' => random_review.author_name,
+                              'review-text' => random_review.text,
+                              'rating' => random_review.rating
+                          })
   end
 end
