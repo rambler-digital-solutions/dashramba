89ae7cdbd142a47f92dd599e06e73c0e85271b91
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-05-24 15:08:30 +0300

    Added widget with number of tests per single project (#7)

--- jobs/jenkins_refresh_job.rb
+++ jobs/jenkins_refresh_job.rb
@@ -4,12 +4,16 @@ SCHEDULER.every '1h', :first_in => 0 do
     tmp_dir = File.expand_path("lib/jenkins/", Dir.pwd)
     project_manager = Infrastructure::ProjectManager.new
     project_manager.obtain_all_projects.each do |project|
-        name = project.jenkins_name
-        next until name != nil
-        output = `python #{tmp_dir}/jenkins_job.py #{name}`.gsub('\'', '"').gsub('u"', '"')
-        result = JSON.parse(output)
-        new_hash = result['items'].map { |item| { label: item["label"], value: item["value"] } }
-        correct_array = correct_array.concat(new_hash)
+      name = project.jenkins_name
+      next until name != nil
+      output = `python #{tmp_dir}/jenkins_job.py #{name}`.gsub('\'', '"').gsub('u"', '"')
+      result = JSON.parse(output)
+      new_hash = result['items']
+      correct_array = correct_array.concat(new_hash)
+
+      separate_widget_name = "jenkins_#{project.jenkins_name}"
+      number_of_tests = new_hash[0]["value"]
+      send_event(separate_widget_name, { current: number_of_tests })
     end
     correct_array = correct_array.sort_by {|hash| hash[:value]}.reverse!
     send_event('jenkins', { items: correct_array })
