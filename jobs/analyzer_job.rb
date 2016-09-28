1e94d243c2e38ad7efd2084d52e356beff1b2c34
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-09-28 15:08:07 +0300

    Added widget with priority issues

--- jobs/analyzer_job.rb
+++ jobs/analyzer_job.rb
@@ -12,11 +12,32 @@ SCHEDULER.every '1d', :first_in => 0 do |job|
 
     service.fetch_analysis_for_bundle_id(project.enterprise_bundle_id)
     model = service.obtain_analysis_model_for_bundle_id(project.enterprise_bundle_id)
-    puts "id=#{project.enterprise_bundle_id}"
     if model != nil
       send_top_issues(model, project)
+      send_priority_issues(model, project)
     end
   end
+end
+
+def send_priority_issues(model, project)
+  widget_name = "analyzer_priority_#{project.jenkins_name}"
+
+  items = [
+      {
+          :label => 'Критичный',
+          :value => model.number_of_first_priority_issues
+      },
+      {
+          :label => 'Средний',
+          :value => model.number_of_second_priority_issues
+      },
+      {
+          :label => 'Низкий',
+          :value => model.number_of_third_priority_issues
+      }
+  ]
+
+  send_event(widget_name,  { items: items })
 end
 
 def send_top_issues(model, project)
@@ -31,5 +52,4 @@ def send_top_issues(model, project)
 
   widget_name = "analyzer_top_#{project.jenkins_name}"
   send_event(widget_name, { items: top_issues })
-
 end
