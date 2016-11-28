980a3cc007a26a0370adba1e423517e65a1dfa49
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-11-28 20:08:18 +0300

    Fixed issue #16

--- jobs/appstore_random_review_job.rb
+++ jobs/appstore_random_review_job.rb
@@ -3,13 +3,17 @@ require_relative '../lib/infrastructure/project_model'
 require_relative '../lib/appstore/review_service'
 require_relative '../lib/appstore/review_model'
 
+LATEST_REVIEW_COUNT = 50
+
 SCHEDULER.every '15s', :first_in => 0 do |job|
   project_manager = Infrastructure::ProjectManager.new
   project_manager.obtain_all_projects.each do |project|
     next unless project.appstore_id != nil
     service = AppStore::ReviewService.new
     reviews = service.obtain_reviews_for_app_id(project.appstore_id)
-    random_review = reviews.sample
+
+    # We work only with latest 50 reviews
+    random_review = reviews[0..LATEST_REVIEW_COUNT - 1].sample
     if random_review != nil
         widget_name = "appstore_review_#{project.appstore_id}"
         send_event(widget_name, {
